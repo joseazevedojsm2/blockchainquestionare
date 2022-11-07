fcaab011fb081b57e61114b72a6b4ca012cfb648
José Azevedo <jose.azevedo@solera.com>
2022-11-07 09:42:02 +0100

    init project

---
+++ src/main/java/com/solera/blockchainquestionare/BlockchainquestionareApplication.java
@@ -0,0 +1,13 @@
+package com.solera.blockchainquestionare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainquestionareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainquestionareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solera/blockchainquestionare/BlockchainquestionareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solera.blockchainquestionare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainquestionareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
